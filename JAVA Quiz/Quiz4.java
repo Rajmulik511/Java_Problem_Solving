4cf40a9f1187e8921bc3e4ed7a8ac24b06b16831
Raj Mulik <86430227+Rajmulik511@users.noreply.github.com>
2023-06-04 00:13:09 +0530

    Add files via upload

---
+++ JAVA Quiz/Quiz4.java
@@ -0,0 +1,51 @@
+import java.util.*;
+class Quiz{
+    public static void main (String[] args){
+        //QUIZ NO 4
+
+        //QUIZ P2
+//        StringBuffer strbulider =new StringBuffer("String");
+//        StringBuffer stringBuffer =new StringBuffer(strbulider);
+//        String str= new String(stringBuffer);
+//        System.out.println(str.replace('t','p'));
+//        System.out.println(str);
+        //QUIZ P3
+//        StringBuilder str =new StringBuilder("Bulider");
+//        str.insert(5,new String("0"));
+//        str.reverse();
+//        System.out.println(str.reverse());
+        //QUIZ P4
+//        StringBuilder str = new StringBuilder(200);
+//        str.append("Bytecode Is Platform Independent");
+//        str.setLength(20);
+//        System.out.println(str);
+        //QUIZ P5
+//        StringBuilder str= new StringBuilder();
+//        str.append("Java_Virtual_Machine");
+//        str.setCharAt(str.charAt(14),'z');
+//        System.out.println(str);    RUNTIME EXCEPTION================
+        //QUUIZ P6
+//        StringBuilder sbvar1=new StringBuilder("Hey");
+//        StringBuilder sbvar2=new StringBuilder("Hey");
+//        if (sbvar1.equals(sbvar2))
+//            System.out.println("equal");
+//        else
+//            System.out.println("not equal");
+        //QUIZ P7
+//        StringBuilder sbvar=new StringBuilder(10);
+//        System.out.println(sbvar);
+        //QUIZ P8
+//        StringBuilder sb = new StringBuilder("Shashi");
+//        sb.insert(6,"t");//
+//        System.out.println(sb);
+        //QUIZ P9
+        StringBuilder sb  =new StringBuilder("Shashi");
+        sb.replace(0,1,"p");
+        System.out.println(sb);
+        //QUIZ P10
+//        StringBuilder obj = new StringBuilder("Hello");
+//        obj.append("All");
+//        obj.append("!! Good Morning");
+//        System.out.println(obj);
+    }
+}
